a4a4e388d495d1306c2e93716d84ea5cf027bee5
ParaHuang <paradox041@gmail.com>
2024-03-07 23:08:16 +0800

    array

---
+++ AidarDemo/src/day5/Main1.java
@@ -0,0 +1,70 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	//check if one number is a narcissistic number
+	public static void main(String[] args) {
+		//every 3-digit number
+		for(int i=100 ; i<1000 ; i++) {
+			int number = i;
+			int ones = number%10;
+			int tens = number%100/10;
+			int hundreds = number/100;
+			int sum = ones*ones*ones + tens*tens*tens + hundreds*hundreds*hundreds;
+			//num:153     1     5    	3
+			//        	  1  +  125 +  27 = 153->sum
+			//num:123	  1		2		3
+			//            1	 +	8	+	27 = 36->sum
+	
+			if(number == sum){
+				System.out.println(number+" is a narcissistic number");
+			}
+		}
+		
+		//initial value, condition , increment
+		int i=100;
+		while(i<1000) {
+			
+			int number = i;
+			int ones = number%10;
+			int tens = number%100/10;
+			int hundreds = number/100;
+			int sum = ones*ones*ones + tens*tens*tens + hundreds*hundreds*hundreds;
+			//num:153     1     5    	3
+			//        	  1  +  125 +  27 = 153->sum
+			//num:123	  1		2		3
+			//            1	 +	8	+	27 = 36->sum
+	
+			if(number == sum){
+				System.out.println(number+" is a narcissistic number");
+			}
+			
+			
+			i++;
+		}
+		
+		
+		
+		
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("enter a 3-digit number:");
+//		int number = scanner.nextInt();
+//		int ones = number%10;
+//		int tens = number%100/10;
+//		int hundreds = number/100;
+//		int sum = ones*ones*ones + tens*tens*tens + hundreds*hundreds*hundreds;
+//		//num:153     1     5    	3
+//		//        	  1  +  125 +  27 = 153->sum
+//		//num:123	  1		2		3
+//		//            1	 +	8	+	27 = 36->sum
+//
+//		if(number == sum){
+//			System.out.println(number+" is a narcissistic number");
+//		}
+	}
+
+}
+
---
+++ AidarDemo/src/day5/Main2.java
@@ -0,0 +1,43 @@
+package day5;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		//1.define an array,and declared the length of this array
+		//type[] aryName = new type[length];
+		
+		//define an array , which can save 7 integer numbers
+		int[] ary = new int[6];
+		ary[0] = 23;
+		ary[1] = 66;
+		ary[2] = 88;
+		ary[3] = 1;
+		ary[4] = 8;
+		ary[5] = 77;	
+		
+//		System.out.println(ary[0]);//the first element of array
+//		System.out.println(ary[1]);
+//		System.out.println(ary[2]);
+//		System.out.println(ary[3]);
+//		System.out.println(ary[4]);
+//		System.out.println(ary[5]);
+		
+		
+		//loop:repeating same or similar stuffs
+		for(int i=0 ; i<ary.length ; i++){//0~5
+			System.out.println(ary[i]);
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
