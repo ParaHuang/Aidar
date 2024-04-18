11c228c838eac438740c1b8b6694f280bf446bad
ParaHuang <paradox041@gmail.com>
2024-04-18 17:56:42 +0800

    HashSet

---
+++ AidarDemo/src/day14/Main1.java
@@ -0,0 +1,62 @@
+package day14;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(11);
+		list.add(22);
+		list.add(33);
+		list.add(44);
+		list.add(33);
+		list.add(22);
+		System.out.println(list.size());
+		System.out.println(list);
+		System.out.println(list.get(3));
+		System.out.println();
+		
+		//1.only save unique data
+		//2.those data are not in order
+		//3.doesn't have index
+		//instantiate
+//		HashSet<Integer> set = new HashSet<Integer>();
+//		TreeSet<Integer> set = new TreeSet<Integer>();		//in natural order
+		LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();		//in add order
+		
+		//add data
+		set.add(11);
+		set.add(22);
+		set.add(33);
+		set.add(44);
+		set.add(33);
+		set.add(22);
+		set.add(1);
+		set.add(3);
+		System.out.println(set.size());	//size
+			
+		for(int x:set) {		//check every element
+			System.out.println(x);
+		}
+		
+		boolean f = set.remove(88);
+		System.out.println(f);
+		System.out.println(set);
+		
+		boolean f2 = set.contains(33);
+		System.out.println(f2);
+	}
+
+}
+
+
+
+
+
+
+
---
+++ AidarDemo/src/day14/Main3.java
@@ -0,0 +1,40 @@
+package day14;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		//List   						Set
+		//they both can save a lot of data
+		//save duplicate data			can't save duplicates
+//		1.You are organizing a small party, which is only allowed 5 people to attend,
+//		but 10 people signed for this party, they are:
+//		Jhon, Jack, Alex, Helan, Cecilia, Mia,Sonia, Tomas, Tyler, Chris.
+//		Now you need to choose 5 of them randomly to attend your party.
+//		Try to create a program to make it come true, in the end, output your guests' name one by one.
+		String[] ary = {"Jhon", "Jack", "Alex", "Helan", "Cecilia", "Mia","Sonia", "Tomas", "Tyler", "Chris"};
+		HashSet<String> set = new HashSet<String>();
+		Random random = new Random();
+		while(set.size()<5) {
+			int index = random.nextInt(10);
+			String name = ary[index];
+			System.out.println(name);
+			set.add(name);
+		}
+		System.out.println(set);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
