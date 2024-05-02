bc9d64810999d11e49df86983ec7c8473e5c98e7
ParaHuang <paradox041@gmail.com>
2024-05-02 23:59:54 +0800

    oop1

---
+++ AidarDemo/src/day16/Book.java
@@ -0,0 +1,32 @@
+package day16;
+
+//1.define a class
+public class Book {
+	//									object			--	class
+	//name author   type   price    --> feature				attribute/field/instance variable
+	private String name;
+	private String author;
+	private String type;
+	private double price;
+	//every variable has a default value
+	//int->0   double->0.0   boolean->false   String->null
+	
+	//assign/set a value to name
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public void setPrice(double p) {
+		if(p<=0) {
+			System.out.println("invalid price");
+		}else {
+			price = p;
+		}
+	}
+	
+	//teaching						--> behavior			method/function
+	public void teaching() {
+		System.out.println("the price of <"+name+"> is "+price+", and it teaches us a lesson");
+	}
+}
+
