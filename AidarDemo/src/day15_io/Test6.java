471cd094b2e7bd59cd52f3fbf4ccc2f7600ac1e0
ParaHuang <paradox041@gmail.com>
2026-01-04 09:21:06 +0800

    io

---
+++ AidarDemo/src/day15_io/Test6.java
@@ -0,0 +1,23 @@
+package day15_io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class Test6 {
+    public static void main(String[] args) throws IOException {
+        /*
+        Arrays:the tool of array
+        Collections:the tool of Collection
+        Files:the tool of file
+        */
+        
+    }
+}
+
+//Consumer： with parameter, no return (need data to do operation, but without result)
+//Supplier: no parameter, with return
+//Predicator: with parameter, return boolean
+//Functional:with parameter, with return
